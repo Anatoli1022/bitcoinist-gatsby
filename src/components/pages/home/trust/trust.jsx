4066a681ae68724ed669a552a12481ca211079d4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-04-07 15:35:33 +0300

    feat: adds section trust

---
+++ src/components/pages/home/trust/trust.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import styles from './trust.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Trust = () => (
+  <section className={cx('trust')}>
+    <div className={cx('container')}>
+      <h2 className={cx('title')}>
+        They <span className={cx('title-span')}> trust us</span>
+      </h2>
+      <div className={cx('wrapper-images')}>
+        <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
+        <StaticImage src="../../../../images/java.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/company.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/js.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/java.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/goes.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/tagline.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/java.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/here.png" alt="" loading="lazy" />
+      </div>
+    </div>
+  </section>
+);
+
+export default Trust;
