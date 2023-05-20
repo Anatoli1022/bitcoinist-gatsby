c851905097b6a46da54afd2fe224b670d71ffae4
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-05-20 04:45:49 +0300

    feat: adds sections for main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
 
+import Advantages from 'components/pages/home/advantages';
 import Web from 'components/pages/home/web';
 import Trust from 'components/pages/home/trust';
+import Projects from 'components/pages/home/projects';
 import Technologies from 'components/pages/home/technologies';
 import Team from 'components/pages/home/team';
 
 const HomePage = () => (
   <Layout>
+    <Advantages />
     <Web />
     <Trust />
+    <Projects />
     <Technologies />
     <Team />
   </Layout>
