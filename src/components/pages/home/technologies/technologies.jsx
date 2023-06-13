361f94bdc65f2f11faf02c3a7057f2b50d5208a6
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-06-14 02:42:26 +0300

    feat: adds section technologies

--- src/components/pages/home/technologies/technologies.jsx
+++ src/components/pages/home/technologies/technologies.jsx
@@ -14,9 +14,9 @@ const Technologies = () => (
       </h2>
       <div className={cx('wrapper-images')}>
         <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
-        <StaticImage src="../../../../images/java.png" alt="" loading="lazy" />
-        <StaticImage src="../../../../images/company.png" alt="" loading="lazy" />
-        <StaticImage src="../../../../images/js.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
+        <StaticImage src="../../../../images/company.png" alt="company" loading="lazy" />
+        <StaticImage src="../../../../images/js.png" alt="java script" loading="lazy" />
         <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
         <StaticImage src="../../../../images/goes.png" alt="" loading="lazy" />
         <StaticImage src="../../../../images/tagline.png" alt="" loading="lazy" />
