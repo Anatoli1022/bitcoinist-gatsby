4008d4bcd0f26c5811625498b890ddf52dca12ce
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-07-18 21:10:06 +0300

    fix: fixes alt for the images

--- src/components/pages/home/trust/trust.jsx
+++ src/components/pages/home/trust/trust.jsx
@@ -15,15 +15,15 @@ const Trust = () => (
       <div className={cx('wrapper-images')}>
         <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
         <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
-        <StaticImage src="../../../../images/company.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/company.png" alt="company" loading="lazy" />
         <StaticImage src="../../../../images/js.png" alt="java script" loading="lazy" />
-        <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/tagline_here.png" alt="company" loading="lazy" />
         <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
-        <StaticImage src="../../../../images/goes.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/goes.png" alt="company" loading="lazy" />
         <StaticImage src="../../../../images/tagline.png" alt="tagline" loading="lazy" />
         <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
         <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
-        <StaticImage src="../../../../images/here.png" alt="" loading="lazy" />
+        <StaticImage src="../../../../images/here.png" alt="company" loading="lazy" />
       </div>
     </div>
   </section>
