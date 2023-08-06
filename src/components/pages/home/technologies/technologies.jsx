b9c151513e9f9f8a64e35a2034015c25bf908696
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-06 23:09:18 +0300

    feat: adds mobile version for section technologies

--- src/components/pages/home/technologies/technologies.jsx
+++ src/components/pages/home/technologies/technologies.jsx
@@ -13,14 +13,18 @@ const Technologies = () => (
         Our <span className={cx('title-span')}> technologies</span>
       </h2>
       <div className={cx('wrapper-images')}>
-        <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
-        <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
-        <StaticImage src="../../../../images/company.png" alt="company" loading="lazy" />
-        <StaticImage src="../../../../images/js.png" alt="java script" loading="lazy" />
-        <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
-        <StaticImage src="../../../../images/goes.png" alt="" loading="lazy" />
-        <StaticImage src="../../../../images/tagline.png" alt="" loading="lazy" />
-        <StaticImage src="../../../../images/here.png" alt="" loading="lazy" />
+        <div>
+          <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
+          <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
+          <StaticImage src="../../../../images/company.png" alt="company" loading="lazy" />
+          <StaticImage src="../../../../images/js.png" alt="java script" loading="lazy" />
+        </div>
+        <div>
+          <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
+          <StaticImage src="../../../../images/goes.png" alt="" loading="lazy" />
+          <StaticImage src="../../../../images/tagline.png" alt="" loading="lazy" />
+          <StaticImage src="../../../../images/here.png" alt="" loading="lazy" />
+        </div>
       </div>
     </div>
   </section>
