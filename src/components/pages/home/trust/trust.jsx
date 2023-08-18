22c9f4b5f9c7247d1c6f28aa942d1b4b7242c153
Anatoli1022 <118132724+Anatoli1022@users.noreply.github.com>
2023-08-19 01:58:49 +0300

    fix: animation in section trust

--- src/components/pages/home/trust/trust.jsx
+++ src/components/pages/home/trust/trust.jsx
@@ -13,19 +13,14 @@ const Trust = () => (
         They <span className={cx('title-span')}> trust us</span>
       </h2>
       <div className={cx('wrapper-images')}>
-        <div>
+        <div className={cx('container-images')}>
           <StaticImage src="../../../../images/css.png" alt="css" loading="lazy" />
-          <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
           <StaticImage src="../../../../images/company.png" alt="company" loading="lazy" />
           <StaticImage src="../../../../images/js.png" alt="java script" loading="lazy" />
           <StaticImage src="../../../../images/tagline_here.png" alt="company" loading="lazy" />
-        </div>
-        <div>
           <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
           <StaticImage src="../../../../images/goes.png" alt="company" loading="lazy" />
           <StaticImage src="../../../../images/tagline.png" alt="tagline" loading="lazy" />
-          <StaticImage src="../../../../images/tagline_here.png" alt="" loading="lazy" />
-          <StaticImage src="../../../../images/java.png" alt="java" loading="lazy" />
           <StaticImage src="../../../../images/here.png" alt="company" loading="lazy" />
         </div>
       </div>
